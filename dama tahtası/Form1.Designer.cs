28c5535286689d76c4bdafa4d4625413e5baaee4
unknown <mefkaryozgat@gmail.com>
2020-04-14 10:35:00 +0300

    ilk yorum

---
+++ dama tahtası/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace yilan_oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_rest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(47, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(520, 420);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(612, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_rest
+            // 
+            this.btn_rest.Enabled = false;
+            this.btn_rest.Location = new System.Drawing.Point(615, 124);
+            this.btn_rest.Name = "btn_rest";
+            this.btn_rest.Size = new System.Drawing.Size(75, 40);
+            this.btn_rest.TabIndex = 2;
+            this.btn_rest.Text = "Yeniden Başla";
+            this.btn_rest.UseVisualStyleBackColor = true;
+            this.btn_rest.Click += new System.EventHandler(this.btn_rest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_rest);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_rest;
+    }
+}
+
---
+++ dama tahtası/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yilan_oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+
+
+
+
+       yilan yilanimiz;
+        Yon yonumuz;
+
+        PictureBox[] pb_yilanparcalari;
+        bool yem_varmi = false;
+        Random rasg = new Random();
+        PictureBox pb_yem;
+        int skor = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Yeni_oyun();
+        }
+        private void Yeni_oyun()
+        {
+            yilanimiz = new yilan();
+            yonumuz = new Yon(-10, 0);
+
+            pb_yilanparcalari = new PictureBox[0];
+            for (int i = 0; i < 3; i++)
+            {
+                Array.Resize(ref pb_yilanparcalari, pb_yilanparcalari.Length + 1);
+                pb_yilanparcalari[i] = Pb_ekle();
+            }
+            timer1.Start();
+            btn_rest.Enabled = false;
+        }
+
+
+        private PictureBox Pb_ekle()
+             
+        {
+            
+            PictureBox pb = new PictureBox();
+            pb.Size = new Size(10, 10);
+            pb.BackColor = Color.GreenYellow;
+            pb.Location = yilanimiz.GetPos(pb_yilanparcalari.Length-1);
+            panel1.Controls.Add(pb);
+            return(pb);
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void pb_guncelle()
+        {
+            for (int i = 0; i < pb_yilanparcalari.Length; i++)
+            {
+                pb_yilanparcalari[i].Location = yilanimiz.GetPos(i);
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            { if (yonumuz._y != 10)
+                { 
+                    yonumuz = new Yon(0, -10); 
+                }
+            }
+            else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                if (yonumuz._y != -10)
+                {
+                    yonumuz = new Yon(0, +10);
+                }
+            }
+            else if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                if (yonumuz._x != 10)
+                {
+                    yonumuz = new Yon(-10, 0);
+                }
+            }
+            else if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                if(yonumuz._x != -10){
+                    yonumuz = new Yon(+10, 0); }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "Skor :" + skor.ToString();
+            yilanimiz.İlerle(yonumuz);
+            pb_guncelle();
+            Yem_olustur();
+            Yem_yedi_mi();
+            yilan_kendine_carpti();
+            Duvarlara_carpma();
+           
+        }
+        public void Yem_olustur()
+        {
+            if (!yem_varmi)
+            {
+                PictureBox pb = new PictureBox();
+                pb.BackColor = Color.Red;
+                pb.Size = new Size(20, 20);
+                pb.Location = new Point(rasg.Next(panel1.Width / 10) * 10, rasg.Next(panel1.Height / 10) * 10);
+                yem_varmi = true;
+                pb_yem = pb;
+                panel1.Controls.Add(pb);
+            }
+
+        }
+        public void Yem_yedi_mi()
+        {
+            if (yilanimiz.GetPos(0) == pb_yem.Location)
+            {
+                yilanimiz.Buyu();
+                Array.Resize(ref pb_yilanparcalari, pb_yilanparcalari.Length + 1);
+                pb_yilanparcalari[pb_yilanparcalari.Length - 1] = Pb_ekle();
+                yem_varmi = false;
+                panel1.Controls.Remove(pb_yem);
+                skor += 10;
+            }
+        }
+        public void yilan_kendine_carpti()
+        {
+            for (int i = 1; i < yilanimiz.Yilan_buyuklugu; i++)
+            {
+                if (yilanimiz.GetPos(0) == yilanimiz.GetPos(i))
+                {
+                    yeniden_basla();
+                }
+            }
+            
+        }
+        public void Duvarlara_carpma()
+        {
+            Point p = yilanimiz.GetPos(0);
+            if(p.X<0||p.X>panel1.Width-10 || p.Y<0 || p.Y > panel1.Height - 10)
+            {
+                yeniden_basla();
+            }
+        }
+        private void yeniden_basla()
+        {
+            timer1.Stop();
+            btn_rest.Enabled = true;
+            MessageBox.Show("Kaybettin Skorun:" + skor.ToString());
+        }
+
+        private void btn_rest_Click(object sender, EventArgs e)
+        {
+
+            skor = 0;
+            panel1.Controls.Clear();
+            Yeni_oyun();
+             panel1.Controls.Add(pb_yem);
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    
+
